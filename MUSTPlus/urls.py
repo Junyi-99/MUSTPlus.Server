8c76efd3c8827941d73d66f3d8ba884908f4e18a
Junyi <hji2009@126.com>
2019-04-29 21:40:56 +0800

    URLS.PY
    Signed-off-by: Junyi <hji2009@126.com>

--- MUSTPlus/urls.py
+++ MUSTPlus/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from API import Authentication
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hash/', Authentication.hash)
 ]
