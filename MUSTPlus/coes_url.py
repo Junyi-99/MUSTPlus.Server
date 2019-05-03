49429e27125eb5167c0258139b2d369679b75966
King Aikov <dzhlong@outlook.com>
2019-05-03 15:46:47 +0800

    增加url的记录

---
+++ MUSTPlus/coes_url.py
@@ -0,0 +1,5 @@
+LOGIN_URL = 'https://coes-stud.must.edu.mo/coes/login.do'
+STUDENTINFO_URL = 'https://coes-stud.must.edu.mo/coes/StudentInfo.do'
+STUDYPLANGROUP_URL = 'https://coes-stud.must.edu.mo/coes/StudyPlanGroup.do'
+LOGOUT_URL = 'https://coes-stud.must.edu.mo/coes/logout.do'
+TABLETIME_URL = 'https://coes-stud.must.edu.mo/coes/AcademicRecordsForm.do'
