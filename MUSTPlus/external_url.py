4ebb3860dd9639813584f3be2c5c44c943b84356
Junyi <hji2009@126.com>
2019-05-06 21:11:15 +0800

    对于文件COES.py的修改： 1、将get_cookie修改为login，将coes_logout修改为logout。统一命名 2、修改get_info函数中的Student查找创建顺序，防止垃圾请求 3、修改login函数，返回值限定到只有一个
    Signed-off-by: Military-Doctor <hji2009@126.com>

--- MUSTPlus/external_url.py
+++
@@ -1,9 +0,0 @@
-LOGIN_URL = 'https://coes-stud.must.edu.mo/coes/login.do'
-STUDENT_INFO_URL = 'https://coes-stud.must.edu.mo/coes/StudentInfo.do'
-STUDY_PLAN_GROUP_URL = 'https://coes-stud.must.edu.mo/coes/StudyPlanGroup.do'
-LOGOUT_URL = 'https://coes-stud.must.edu.mo/coes/logout.do'
-TIME_TABLE_URL = 'https://coes-stud.must.edu.mo/coes/AcademicRecordsForm.do'
-
-INTRANET_LOGIN_URL = 'https://intranet.must.edu.mo/student/LoginServlet'
-INTRANET_LOGOUT_URL = 'https://intranet.must.edu.mo/student/Logout'
-# .
