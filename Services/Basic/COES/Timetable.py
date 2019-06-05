0465c2a369a619e9b960aa8b1f9286143e8f8dbb
Military-Doctor <hji2009@126.com>
2019-06-06 01:05:15 +0800

    [FIX] BUG Fixed in Timetable. （Reverse iter）
    Signed-off-by: Military-Doctor <hji2009@126.com>

--- Services/Basic/COES/Timetable.py
+++ Services/Basic/COES/Timetable.py
@@ -57,11 +57,11 @@ def get_timetable(html_source: str) -> list:
 
     course_list = re.findall(r'timetable.add\([\s\S]*?\);', html_source)
 
-
     for each_course in course_list:
         sp2 = each_course.replace("timetable.add(", '').replace("'", '').replace('\r', '').replace('\n ', '').split(',')
 
-        for i in range(len(month)):  # 将中文月份转换到数字
+        for i in range(len(month) - 1, -1, -1):  # 将中文月份转换到数字 这里要反向迭代，因为 '十二月' 会在正向迭代的时候 被 '二月' 先替换
+            print(month[i], i + 1)
             sp2[8] = sp2[8].replace(month[i], "%d-" % (i + 1))
 
         pos_plus = sp2[8].find('+')
